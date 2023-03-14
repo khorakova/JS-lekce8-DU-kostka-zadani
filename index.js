63a109ceb48dbf8b28af84e037c3fa53ca327ecc
khorakova <horakoak@gmail.com>
2023-03-14 17:35:07 +0100

    reseni

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log('funguju!');
+
+let side = 2
+
+const increaseNumberOfDotsOnDice = () => {
+    let dice = document.querySelector(".dice")
+    let diceSrc = "img/side"+side+".svg"
+    if (side === 6) {
+        dice.src = diceSrc
+        side = 1
+    } else {
+        dice.src = diceSrc
+        side = side + 1
+    }
+}
+
+document.addEventListener('keypress', increaseNumberOfDotsOnDice)
+
